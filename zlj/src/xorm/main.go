acc7776a9d7e76c34012620ff4f3cd79b452a925
zlj1049283741 <13637305640@163.com>
2020-01-16 14:52:17 +0800

    test

---
+++ zlj/src/xorm/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/core"
+	"github.com/go-xorm/xorm"
+	"time"
+)
+
+
+type User1 struct {
+	Id       int       `xorm:"not null pk autoincr INT(11)"`
+	Username string    `xorm:"not null VARCHAR(32)"`
+	Birthday time.Time `xorm:"DATE"`
+	Sex      string    `xorm:"CHAR(1)"`
+	Address  string    `xorm:"VARCHAR(256)"`
+}
+
+
+func main()  {
+
+	//创建orm引擎
+	engine, err := xorm.NewEngine("mysql", "root:root@/test?charset=utf8")
+
+	if err!=nil{
+		fmt.Println(err)
+		return
+	}
+	//连接测试
+	if err := engine.Ping(); err!=nil{
+		fmt.Println(err)
+		return
+	}
+
+	//日志打印SQL
+	engine.ShowSQL(true)
+
+	//设置连接池的空闲数大小
+	engine.SetMaxIdleConns(5)
+	//设置最大打开连接数
+	engine.SetMaxOpenConns(5)
+
+
+	//名称映射规则主要负责结构体名称到表名和结构体field到表字段的名称映射
+	engine.SetTableMapper(core.SnakeMapper{})
+
+	//增
+	//user := new(User)
+	//user.Username="tyming"
+	//affected,err := engine.Insert(user)
+	//fmt.Println(affected)
+
+	//删
+	//user := new(User)
+	//user.Username="tyming"
+	//affected_delete,err := engine.Delete(user)
+	//fmt.Println(affected_delete)
+
+	//改
+	//user := new(User)
+	//user.Username="tyming"
+	//affected_update,err := engine.Id(1).Update(user)
+	//fmt.Println(affected_update)
+
+	//查
+	user := new(User1)
+	//result,err := engine.Id(1).Get(user)
+	result,err := engine.Where("id=?",1).Get(user)
+	fmt.Println(result)
+	fmt.Println(*user)
+}
